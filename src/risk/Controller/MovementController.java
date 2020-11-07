bf22f1ca42203b1b55f7fd0528cf858202cbb935
Rahel Gunaratne <rahel.gunaratne@gmail.com>
2020-11-07 12:37:52 -0500

    deleted useless code

--- src/risk/Controller/MovementController.java
+++ src/risk/Controller/MovementController.java
@@ -1,78 +1,9 @@
 package risk.Controller;
 
-/*
-import risk.Model.Country;
-import risk.Model.GameModel;
-<<<<<<< HEAD
-
-import javax.swing.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-
-public class MovementController {
-
-    private  Country firstCountry, secondCountry;
-    private  Controller controller;
-    private HashMap<String, Country> checkedCountries = new HashMap<>();
-
-    public MovementController(GameModel gameModel, JFrame frame){
-        this.controller = controller;
-    }
-
-    public boolean isFirstCountryValidChoice(Country firstCountry){
-        if (controller.getGameModel().getCurrentPlayer().getIndex() != firstCountry.getPlayer().getIndex())
-            return false;
-        return true;
-    }
-
-    public void setFirstCountry(Country firstCountry){
-        this.firstCountry = firstCountry;
-    }
-
-    public boolean isSecondCountryValidChoice(Country secondCountry){
-        if (controller.getGameModel().getCurrentPlayer().getIndex() != firstCountry.getPlayer().getIndex())
-            return false;
-
-        checkedCountries.clear();
-        if (!areCountriesConnected(firstCountry))
-            return false;
-
-        return true;
-    }
-
-
-    public void setSecondCountry(Country secondCountry){
-        this.secondCountry = secondCountry;
-    }
-
-    //get
-    public boolean areCountriesConnected(Country currentCountry){
-        if (currentCountry.getName().equals(secondCountry))
-            return true;
-
-        if (checkedCountries.size() == controller.getGameModel().getCountries().size())
-            return false;
-
-        for (String countryName : currentCountry.getNeighbours()){
-            Country c = controller.getGameModel().getCountry(countryName);
-            if (c.getPlayer().getIndex() != firstCountry.getPlayer().getIndex())
-                continue;
-
-            if (checkedCountries.containsKey(countryName))
-                continue;
-
-            areCountriesConnected(c);
-        }
-
-        return false;
-    }
-*/
-
 import risk.Model.Country;
 import risk.Model.GameModel;
 
 import javax.swing.*;
-import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MovementController {
